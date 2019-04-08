3aff98d5d3086b5c2b2007eaac0f1d7f2be1de8f
VladimirPritkov <uksuss9999@gmail.com>
2019-04-08 18:58:03 +0100

    Dobavili faili

---
+++ js/java.js
@@ -0,0 +1,17 @@
+let name = 'vova';
+
+console.log(name);
+
+if (1)
+{
+  let name = "artur"
+  console.log(name);
+}
+
+console.log(name);
+
+
+const A = {asd:999}
+console.log(A);
+A.asd = 123;
+console.log(A);
---
+++ js/java2.js
@@ -0,0 +1,3 @@
+
+let x = 5;// это лучше чем var
+alert("hello");
